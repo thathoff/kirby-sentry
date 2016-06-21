ccc2c326f0e59933d66194b9e6a569c02aee2c41
Pascal Küsgen <pascal@pascalmh.de>
2016-06-21 16:05:53 +0200

    feat: add disabled option

--- sentry-kirby.php
+++ sentry-kirby.php
@@ -1,5 +1,5 @@
 <?php
-if ($sentryDsn = c::get('sentry.dsn', false)) {
+if ($sentryDsn = c::get('sentry.dsn', false) && !c::get('sentry.disabled', false)) {
     require_once __DIR__ . '/sentry-php/lib/Raven/Autoloader.php';
     Raven_Autoloader::register();
 
@@ -10,5 +10,7 @@ if ($sentryDsn = c::get('sentry.dsn', false)) {
     $error_handler->registerErrorHandler();
     $error_handler->registerShutdownFunction();
 } else {
-    trigger_error('Sentry DSN not defined', E_USER_NOTICE);
+    if (!$sentryDsn) {
+        trigger_error('Sentry DSN not defined', E_USER_NOTICE);
+    }
 }
