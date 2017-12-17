d54143f80c194074bc8c65bb9ae6ef5417d5c8ca
Manuel Steinberg <github@manuel-steinberg.de>
2017-12-17 20:39:50 +0100

    Rename sentry-kirby.php to kirby-sentry.php

---
+++ kirby-sentry.php
@@ -0,0 +1,28 @@
+<?php
+
+    if (($sentryDsn = c::get('sentry.dsn', false)) && !c::get('sentry.disabled', false)) {
+        require_once __DIR__ . '/sentry-php/lib/Raven/Autoloader.php';
+        Raven_Autoloader::register();
+
+        $client = new Raven_Client($sentryDsn);
+
+        if ($user = site()->user()) {
+            $client->user_context(array(
+                'id' => $user->username(),
+                'username' => $user->firstName() . ' ' . $user->lastName(),
+                'email' => $user->email(),
+            ));
+        }
+
+        $error_handler = new Raven_ErrorHandler($client);
+        $error_handler->registerExceptionHandler();
+        $error_handler->registerErrorHandler();
+        $error_handler->registerShutdownFunction();
+        
+
+    } else {
+        if (!$sentryDsn) {
+            trigger_error('Sentry DSN not defined', E_USER_NOTICE);
+        }
+    }
+
